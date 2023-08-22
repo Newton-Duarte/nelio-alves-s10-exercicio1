5b48dbd669b836868c37dd8d34d69ee943101351
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-08-22 20:19:28 -0300

    first commit

---
+++ Entities/OutsourcedEmployee.cs
@@ -0,0 +1,21 @@
+class OutsourcedEmployee : Employee
+{
+  public double AdditionalCharge { get; set; }
+
+  public OutsourcedEmployee()
+  {
+
+  }
+
+  public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge)
+    : base(name, hours, valuePerHour)
+  {
+    AdditionalCharge = additionalCharge;
+  }
+
+  public override double Payment()
+  {
+    var totalPayment = base.Payment();
+    return totalPayment + AdditionalCharge * 1.1;
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿System.Console.Write("Enter the number of employees: ");
+var answer = int.Parse(Console.ReadLine());
+var counter = 1;
+
+List<Employee> employees = new();
+
+while (counter <= answer)
+{
+  System.Console.WriteLine($"Employee #{counter} data:");
+  System.Console.Write("Outsourced (y/n)? ");
+  var outsourced = Console.ReadLine();
+
+  System.Console.Write("Name: ");
+  var name = Console.ReadLine();
+  
+  System.Console.Write("Hours: ");
+  var hours = int.Parse(Console.ReadLine());
+  
+  System.Console.Write("Value per hour: ");
+  var valuePerHour = double.Parse(Console.ReadLine());
+  
+  if (outsourced.ToLower() == "y")
+  {
+    System.Console.Write("Additional charge: ");
+    var additionalCharge = double.Parse(Console.ReadLine());
+    var employee = new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge);
+
+    employees.Add(employee);
+  }
+  else
+  {
+    var employee = new Employee(name, hours, valuePerHour);
+    employees.Add(employee);
+  }
+
+  counter++;
+}
+
+System.Console.WriteLine("PAYMENTS:");
+foreach(var employee in employees)
+{
+  System.Console.WriteLine($"{employee.Name} - {employee.Payment():F2}");
+}
